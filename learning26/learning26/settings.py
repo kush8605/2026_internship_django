3fb3e4d8b4024c15ff0370b2dd3bb31059be432a
kush pandya <kushpandya865@gmail.com>
2026-02-02 22:10:38 +0530

    Migration done and superuser created

--- learning26/learning26/settings.py
+++ learning26/learning26/settings.py
@@ -72,12 +72,18 @@ WSGI_APPLICATION = 'learning26.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'learning26',
+        'USER': 'rajesh.pandya',  # Change to 'postgres' if Step 1 said so
+        'PASSWORD': '',           # Usually blank for Postgres.app
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+  
 
 
 # Password validation
