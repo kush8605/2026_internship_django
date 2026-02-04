9d97b7b477ea8ada552a6a74e209e62d9f40d068
kush pandya <kushpandya865@gmail.com>
2026-02-04 12:16:15 +0530

    Create Models

--- learning26/student/admin.py
+++ learning26/student/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import student,product,car
 
 # Register your models here.
+admin.site.register(student)
+admin.site.register(product)
+admin.site.register(car)
--- learning26/student/models.py
+++ learning26/student/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model):
+    studentname=models.CharField(max_length=100)
+    studentage=models.IntegerField()
+    studentcity=models.CharField(max_length=100)
+    studentemail=models.EmailField(null=True)
+
+    # meta class
+    class Meta:
+        db_table="student"
+
+class product(models.Model):
+    productname=models.CharField(max_length=100)
+    productprice=models.IntegerField()
+    productdescription=models.TextField(max_length=100)
+    productstock=models.PositiveIntegerField()
+    productColor = models.CharField(max_length=20,null=True)
+    productStatus = models.BooleanField(default=True)
+
+    class Meta:
+        db_table="product"     
+
+class car(models.Model):
+    carname=models.CharField(max_length=100)
+    carmodel=models.CharField(max_length=100)
+    carprice=models.PositiveIntegerField()
+
+    class Meta:
+        db_table="car"
